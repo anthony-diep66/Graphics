21fe1c48b831c6327c17c2a36246044b9b8072ea
Anthony Diep <anthonydiep@u.boisestate.edu>
2023-01-28 22:12:34 -0700

    added more util functions

---
+++ intro/color.h
@@ -0,0 +1,17 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+#include "vec3.h"
+
+#include <iostream>
+using namespace std;
+void write_color(ostram &out, color pixel_color)
+{
+    out << static_cast<int>(255.999 * pixel_color.x()) << ' '
+        << static_cast<int>(255.999 * pixel_color.y()) << ' '
+        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+
+}
+
+#endif
+
