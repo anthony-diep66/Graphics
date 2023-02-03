12feca513990b8de174abce3b1ee7b37c97db9a9
Anthony Diep <anthonydiep@u.boisestate.edu>
2023-02-02 23:01:25 -0700

    linear blend background

--- intro/color.h
+++ intro/color.h
@@ -4,12 +4,12 @@
 #include "vec3.h"
 
 #include <iostream>
-using namespace std;
-void write_color(ostram &out, color pixel_color)
+
+void write_color(std::ostream &out, color pixel_color)
 {
     out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-        << static_cast<int>(255.999 * pixel_color.y()) << ' '
-        << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+    << static_cast<int>(255.999 * pixel_color.y()) << ' '
+    << static_cast<int>(255.999 * pixel_color.z()) << '\n';
 
 }
 
