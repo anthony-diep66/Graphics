fee8f609930d490885968f30835acf2bda808c24
Anthony Diep <anthonydiep@u.boisestate.edu>
2023-02-19 00:10:12 -0700

    successful merging of funcs

--- ray.cpp
+++ ray.cpp
@@ -24,27 +24,6 @@ point3 ray::at(double t) const
 {
     return orig + t * dir;
 }
+//unused
 extern Color ray::ray_color()
-{
-    Sphere s(point3(0,0,-1), 0.5);
-    s.color = Color(1,0,1);
-    double t = s.hit(*this);
-    if( t > 0 ) //hits
-    {
-        vec3 N = unit_vector(at(t) - vec3(0, 0, -1));
-        point3 light_source = unit_vector(vec3(-1,-1,-1));
-        double d = dot(N, -light_source);
-        if( d < 0 )
-            d = 0;
-        //Color c = 0.5 * (N + point3(1, 1, 1));
-        Color c = s.color;
-        c *= d;
-        return c;
-    }
-    else //doesn't hit
-    {
-        t = 1 + (dir.y() * 0.5);
-        return (1.0-t) * Color(1.0,1.0,1.0) + t*Color(0.5,0.7,1.0);
-    }
-    
-}
+{   }
